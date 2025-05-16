11cb42cb83f6e98da681d2228375e8e7ec7753aa
Mahopeok <hopemaha11@gmail.com>
2025-05-16 06:23:27 +0100

    Improving UX

--- src/index.js
+++ src/index.js
@@ -14,13 +14,13 @@ function generateRecipe(event) {
 
   let userInput = document.querySelector("#user-input");
   let apiKey = "bd13391t3d87a406o7b70e7f7fa17f8e";
-  let prompt = `User instruction: Generate a Nigerian dish recipe of ${userInput.value}`;    
-  let context = "You are a professional Nigerian Chef with recognised and celerated recipes.Your mision is to generate a recipe in basic HTML format. Make sure to follow the user instructions and sign at the end of the recipe with 'Shecodes AI' in <strong> tag."; 
+  let prompt = `User instruction: Generate a Nigerian dish recipe for ${userInput.value}`;    
+  let context = "You are a professional Nigerian Chef with recognised and celerated recipes. Generate your best recipes in a basic HTML format without including the word 'HTML'. Make sure to follow the user instructions and sign at the end of the recipe with 'Shecodes AI' in <strong> tag."; 
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let recipeElement = document.querySelector("#recipe");
     recipeElement.classList.remove("hidden");
-    recipeElement.innerHTML = `<div class="blink">⏳Generating your recipe about ${userInput.value}...<div/>`;
+    recipeElement.innerHTML = `<div class="blink">⏳Generating your recipe for ${userInput.value}...<div/>`;
 axios.get(apiUrl).then(displayRecipe);
 
 
